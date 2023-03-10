2222492b04daa7964fb64865a44e07628ded1d17
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-03-10 02:57:32 -0300

    Integrando com o web

--- controller/ClienteController.js
+++ controller/ClienteController.js
@@ -4,19 +4,31 @@ const cliente = require('../model/Cliente');
 
 const router = express.Router();
 
+const existe = router.get('/cliente/:email', (req, res) => {
+  const { email } = req.params;
+
+  cliente.findAll({ where: { email } }).then((usuario) => {
+    res.status(200).json(usuario);
+  });
+});
+
 router.post('/cliente', (req, res) => {
   const { nome, sobrenome, email, celular } = req.body;
 
-  cliente
-    .create({
-      nome: nome,
-      sobrenome: sobrenome,
-      email: email,
-      celular: celular,
-    })
-    .then((cliente) => {
-      res.status(200).json(cliente);
-    });
+  if (existe == true) {
+    cliente
+      .create({
+        nome: nome,
+        sobrenome: sobrenome,
+        email: email,
+        celular: celular,
+      })
+      .then((cliente) => {
+        res.status(200).json('Usuário cadastrado');
+      });
+  } else {
+    return res.status(200).json('Usuário já existe');
+  }
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 const clienteController = require('./controller/ClienteController');
 app.use('/', clienteController);
 
